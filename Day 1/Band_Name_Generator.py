be26377bfda7dd6321b8b91c917809907773ef3e
shannon-mulmat <slattery.shannon@gmail.com>
2024-11-19 14:21:32 -0600

    Create Band_Name_Generator.py

---
+++ Day 1/Band_Name_Generator.py
@@ -0,0 +1,15 @@
+"""
+Description:
+1 .Create a greeting for your program.
+2. Ask the user for the city that they grew up in and store it in a variable.
+3. Ask the user for the name of a pet and store it in a variable.
+4. Combine the name of their city and pet and show them their band name.
+
+Completed 11/16/2024
+"""
+
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
+# print(f"Your band name could be {city} {pet}")
