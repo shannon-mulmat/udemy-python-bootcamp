ec46580653e40611f2f18cc267567db42d141ff1
shannon-mulmat <slattery.shannon@gmail.com>
2024-11-19 14:30:42 -0600

    Update Band_Name_Generator.py

--- Day 1/Band_Name_Generator.py
+++ Day 1/Band_Name_Generator.py
@@ -1,6 +1,6 @@
 """
 Description:
-1 .Create a greeting for your program.
+1. Create a greeting for your program.
 2. Ask the user for the city that they grew up in and store it in a variable.
 3. Ask the user for the name of a pet and store it in a variable.
 4. Combine the name of their city and pet and show them their band name.
