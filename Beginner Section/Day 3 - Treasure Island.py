8399e662bd4bb260591f513b683c6c42e218ef0b
shannon-mulmat <slattery.shannon@gmail.com>
2024-11-19 18:01:27 -0600

    Update Day 3 - Treasure Island.py

--- Beginner Section/Day 3 - Treasure Island.py
+++ Beginner Section/Day 3 - Treasure Island.py
@@ -9,7 +9,7 @@ Topics Covered:
 Project Description:
 1. Create a script based on the flowchart: https://viewer.diagrams.net/?highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D
 
-Completed: 
+Completed: 11/19/2024
 """
 
 print('''
