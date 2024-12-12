703e9b32d6a25fecd6985830c47a4a3fbed51e37
shannon-mulmat <slattery.shannon@gmail.com>
2024-12-12 13:26:01 -0600

    Update Day 7 - Hangman Game.py

--- Beginner Section/Day 7 - Hangman Game.py
+++ Beginner Section/Day 7 - Hangman Game.py
@@ -1,1 +1,17 @@
+"""
+Topics Covered:
+1. Chosing random value from a list
+2. User input --> variable
+3. If statements
+4. For loops
+5. While loops
 
+Project Description:
+1. Select a random word from a list of words, example "camel"
+2. Print out a string of underscores that is the same length as the chosen word, example "_ _ _ _ _"
+3. Ask the user to guess a letter, if that letter is in the word then update the string of underscores with the guessed letter in the correct position. 
+   If the guess is wrong, the user loses a life. Either way, they are asked to guess another letter. Example "c _ _ _ _", "c _ _ e _", "c _ m e _", etc.
+4. If the user guesses all letters in the word before they lose all their lives, they win! Otherwuse, they lose.
+
+Completed: 
+"""
