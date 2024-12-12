6246f723f2f3a435715b583424a8f5351bbb0db9
shannon-mulmat <slattery.shannon@gmail.com>
2024-12-12 15:06:54 -0600

    Update Day 7 - Hangman Game.py

--- Beginner Section/Day 7 - Hangman Game.py
+++ Beginner Section/Day 7 - Hangman Game.py
@@ -11,7 +11,7 @@ Project Description:
 2. Print out a string of underscores that is the same length as the chosen word, example "_ _ _ _ _"
 3. Ask the user to guess a letter, if that letter is in the word then update the string of underscores with the guessed letter in the correct position. 
    If the guess is wrong, the user loses a life. Either way, they are asked to guess another letter. Example "c _ _ _ _", "c _ _ e _", "c _ m e _", etc.
-4. If the user guesses all letters in the word before they lose all their lives, they win! Otherwuse, they lose.
+4. If the user guesses all letters in the word before they lose all their lives, they win! Otherwise, they lose.
 
 Completed: 12/12/2024
 """
