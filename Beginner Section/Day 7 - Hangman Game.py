d805d4a5106defff07c295660c277f46f0b28ea1
shannon-mulmat <slattery.shannon@gmail.com>
2024-12-12 15:05:30 -0600

    Update Day 7 - Hangman Game.py

--- Beginner Section/Day 7 - Hangman Game.py
+++ Beginner Section/Day 7 - Hangman Game.py
@@ -15,3 +15,60 @@ Project Description:
 
 Completed: 
 """
+import random
+from hangman_words import word_list
+from hangman_art import logo, stages
+
+print(logo)
+
+lives = 6
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+guessed_letters = []
+
+while not game_over:
+    print(f"**************************** YOU HAVE {lives} LIVES LEFT ****************************")
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if guess not in chosen_word and guess not in guessed_letters:
+        lives -= 1
+        print(f"You guessed '{guess}', that letter is not in the word. You lose a life.")
+    elif guess not in chosen_word and guess in guessed_letters:
+        print(f"You've already guessed the letter '{guess}', you will not lose a life for that.")
+    elif guess in chosen_word and guess in guessed_letters:
+        print(f"You've already guessed the letter '{guess}'.")
+
+    if lives == 0:
+        game_over = True
+        print(f"*********************** The word was {chosen_word} - YOU LOSE **********************")
+
+    if "_" not in display:
+        game_over = True
+        print("**************************** YOU WIN ****************************")
+
+    print(stages[lives])
+
+    guessed_letters.append(guess)
