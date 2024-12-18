7ec987b7e61c0e9c42e0865fd55f9d0fee87bad3
shannon-mulmat <slattery.shannon@gmail.com>
2024-12-17 18:01:12 -0600

    Update Day 8 - Caesar Cipher.py

--- Beginner Section/Day 8 - Caesar Cipher.py
+++ Beginner Section/Day 8 - Caesar Cipher.py
@@ -1,7 +1,18 @@
+"""
+Topics Covered:
+1. Functions with inputs
+2. Positional vs Keyword arguments
 
+Project Description:
+1. Ask user if they want to "encode" or "decode" a message
+2. Ask the user for the message itself
+3. Ask the user for the amount each letter needs to be shifted in the end result
+4. The code will take the user arguments and either encode or decode the provided message by shifting each letter n indices within the provided alphabet list.
+   Encoding will shift the letters in a positive direction (forwards), while decoding will shift the letters in a negative direction (backwards).
+   Example: encode "ab" by 2 indices == "cd"; decode "cd" by 2 indices == "ab"
 
-
-
+Completed: 12/17/2024
+"""
 from art import logo
 
 print(logo)
