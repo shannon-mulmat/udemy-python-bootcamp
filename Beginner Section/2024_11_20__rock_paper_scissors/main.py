77e190256f1b294bc76aa1540923c1bd72615688
shannon-mulmat <slattery.shannon@gmail.com>
2025-01-03 13:33:59 -0600

    Update main.py

--- Beginner Section/2024_11_20__rock_paper_scissors/main.py
+++ Beginner Section/2024_11_20__rock_paper_scissors/main.py
@@ -12,33 +12,7 @@ Project Description:
 Completed: 11/20/2024
 """
 import random
-
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
+from art import rock, paper, scissors
 
 game_list = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors. "))
