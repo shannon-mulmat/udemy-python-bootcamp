d894a462c2d99c1642fae1235af7de140ff347f2
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-03 15:15:36 -0600

    Create scoreboard.py

---
+++ Intermediate Section/2025_03_03__frogger/scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.goto(-260, 260)
+        self.update_scoreboard()
+
+    def level_up(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center", font=FONT)
