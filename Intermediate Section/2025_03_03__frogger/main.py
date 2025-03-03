8c96e8340e7ea697c191743ec9c5e97f7c31317e
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-03 15:14:45 -0600

    Create main.py

---
+++ Intermediate Section/2025_03_03__frogger/main.py
@@ -0,0 +1,45 @@
+"""
+Project Description:
+- Create the game Frogger using the Turtle module
+
+Completed: 3/3/2025
+"""
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.title("Turtle Frogger")
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # detect turtle collision with car
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    # detect successful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.level_up()
+
+screen.exitonclick()
