62a81b7a74efba70b4221a275cd065fd278d7123
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-10 17:24:18 -0500

    Update and rename main.py to main.py

--- Intermediate Section/2025_03_10__us_states_game/main.py
+++ Intermediate Section/2025_03_10__us_states_game/main.py
@@ -7,7 +7,7 @@ Topics Covered:
 Project Description:
 1. Build a game where the user guesses the 50 US states
 
-Completed: 3/9/2025
+Completed: 3/10/2025
 """
 import turtle
 import pandas as pd
@@ -25,17 +25,15 @@ states_list = data.state.to_list()
 already_guessed = []
 correct_guesses = 0
 
-game_is_on = True
-while game_is_on:
+while len(already_guessed) < 50:
     answer_state = screen.textinput(title=f"{str(correct_guesses)}/50 States Correct", prompt="Guess a state:").title()
+
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in states_list:
-            if state not in already_guessed:
-                states_to_learn.append(state)
+        states_to_learn = [state for state in states_list if state not in already_guessed]
         df = pd.DataFrame(states_to_learn)
         df.to_csv("states_to_learn.csv")
         break
+
     if answer_state in states_list and answer_state not in already_guessed:
         already_guessed.append(answer_state)
         state_row = data[data.state == answer_state]
