bafa02f68f0c065284a4910877b65c3894e3d2ae
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-18 16:14:23 -0500

    Update main.py

--- Intermediate Section/2025_03_10__nato_alphabet/main.py
+++ Intermediate Section/2025_03_10__nato_alphabet/main.py
@@ -10,13 +10,33 @@ Ex. Shannon
 ['Sierra', 'Hotel', 'Alfa', 'November', 'November', 'Oscar', 'November']
 
 Completed: 3/10/2025
+UPDATED: 3/18/2025
+- Added error handling (try/except/else)
 """
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
+import pandas as pd
+"""
+# Original code:
 nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for index, row in nato_df.iterrows()}
+print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ").upper()
 word_list = [nato_dict[letter] for letter in word]
 print(word_list)
+"""
+# Updated code:
+nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter:row.code for index, row in nato_df.iterrows()}
+print(nato_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        word_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter only letters of the alphabet.")
+        generate_phonetic()
+    else:
+        print(word_list)
+
+generate_phonetic()
