d1f1d20e0aeaae4c3ed408ba676199d2d78dee90
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-21 13:57:32 -0500

    Create main.py

---
+++ Intermediate Section/2025_03_21__automated_birthday_wisher/main.py
@@ -0,0 +1,10 @@
+"""
+Topics Covered:
+1. SMTP - sending emails via Python
+2. Datetime module
+
+Project Description:
+- Create a program that automatically sends Happy Birthday emails to friends and family on their birthday
+
+Completed: 3/21/2025
+"""
