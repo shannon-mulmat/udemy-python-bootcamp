8f9ed5db9c1e99e88e12851f8a53e67be79e9e20
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-21 13:58:04 -0500

    Create warmup.py

---
+++ Intermediate Section/2025_03_21__automated_birthday_wisher/warmup.py
@@ -0,0 +1,25 @@
+import smtplib
+import datetime as dt
+import random
+
+GMAIL_EMAIL = "smpythonproject@gmail.com"
+GMAIL_APP_PASSWORD = "yarxbvegquzkuxiw"
+YAHOO_EMAIL = "smpythonproject@yahoo.com"
+YAHOO_PASSWORD = "udemy-python-project"
+
+today = dt.datetime.now()
+day_of_week = today.weekday()
+
+if day_of_week == 4:
+    with open("quotes.txt") as quote_file:
+        quotes_list = [line.strip("\n") for line in quote_file.readlines()]
+        random_quote = random.choice(quotes_list)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=GMAIL_EMAIL, password=GMAIL_APP_PASSWORD)
+        connection.sendmail(
+            from_addr=GMAIL_EMAIL,
+            to_addrs=YAHOO_EMAIL,
+            msg=f"Subject:Happy Friday!\n\n{random_quote}"
+        )
