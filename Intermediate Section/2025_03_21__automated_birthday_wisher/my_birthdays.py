7a77fe161f2837aff6a4f39e6962d86850fe1932
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-22 15:36:52 -0500

    Create my_birthdays.py

---
+++ Intermediate Section/2025_03_21__automated_birthday_wisher/my_birthdays.py
@@ -0,0 +1,9 @@
+YAHOO_EMAIL = "smpythonproject@yahoo.com"
+
+my_birthdays = [
+    ["name", "email", "year", "month", "day"],
+    ["Shannon", YAHOO_EMAIL, 1994, 9, 29],
+    ["Ian", YAHOO_EMAIL, 1994, 4, 14],
+    ["Today", YAHOO_EMAIL, 2025, 3, 22],
+    ["Tomorrow", YAHOO_EMAIL, 2025, 3, 23]
+]
