fa574a077ebab72b266ab5eaf1d60f187a9389ef
shannon-mulmat <slattery.shannon@gmail.com>
2025-03-24 12:56:19 -0500

    Update warmup.py

--- Intermediate Section/2025_03_24__iss_overhead_notifier/warmup.py
+++ Intermediate Section/2025_03_24__iss_overhead_notifier/warmup.py
@@ -1,3 +1,6 @@
+"""
+How to get latitude and longitude for the ISS's current position API response
+"""
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -10,3 +13,31 @@ longitude = data["iss_position"]["longitude"]
 iss_position = (latitude, longitude)
 
 print(iss_position)
+
+"""
+Random Kanye quote API response
+"""
+from tkinter import *
+import requests
+
+def get_quote():
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
+    kanye_quote = response.json()["quote"]
+    canvas.itemconfig(quote_text, text=kanye_quote)
+
+window = Tk()
+window.title("Kanye Says...")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=300, height=414)
+background_img = PhotoImage(file="background.png")
+canvas.create_image(150, 207, image=background_img)
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+canvas.grid(row=0, column=0)
+
+kanye_img = PhotoImage(file="kanye.png")
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button.grid(row=1, column=0)
+
+window.mainloop()
