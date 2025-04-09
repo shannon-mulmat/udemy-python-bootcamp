d086cddcd5d1476457bede6f945ec8d7f7593298
shannon-mulmat <slattery.shannon@gmail.com>
2025-04-09 14:48:04 -0500

    Create notification_manager.py

---
+++ Intermediate+ Section/2025_04_09__flight_club/notification_manager.py
@@ -0,0 +1,25 @@
+import smtplib
+GMAIL_EMAIL = "smpythonproject@gmail.com"
+GMAIL_APP_PASSWORD = "yarxbvegquzkuxiw"
+YAHOO_EMAIL = "smpythonproject@yahoo.com"
+
+class NotificationManager:
+
+    def __init__(self):
+        # Retrieve environment variables only once
+        self.to_email = YAHOO_EMAIL
+        self.email = GMAIL_EMAIL
+        self.email_password = GMAIL_APP_PASSWORD
+        # Set up Twilio Client and SMTP connection
+        self.connection = smtplib.SMTP("smtp.gmail.com")
+
+    def send_emails(self, email_list, email_body):
+        with self.connection:
+            self.connection.starttls()
+            self.connection.login(user=self.email, password=self.email_password)
+            for email in email_list:
+                self.connection.sendmail(
+                    from_addr=self.email,
+                    to_addrs=self.to_email,
+                    msg=f"Subject:New Low Price Flight!\n\n{email_body}".encode('utf-8')
+                )
